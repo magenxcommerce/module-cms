a485d2e18705a6d50fea0abe3bc831b0bb5625a7
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 15:28:15 -0700

    Magento Fork initial commit

--- ViewModel/Page/Grid/UrlBuilder.php
+++ ViewModel/Page/Grid/UrlBuilder.php
@@ -103,7 +103,7 @@ class UrlBuilder
             StoreManagerInterface::PARAM_NAME => $store,
             ActionInterface::PARAM_NAME_URL_ENCODED => $this->urlEncoder->encode($href)
         ];
-        if (null !== $storeView && $storeView->getCode() !== $store) {
+        if ($storeView->getCode() !== $store) {
             $query['___from_store'] = $storeView->getCode();
         }
 
